8292a80cd70003997642dcead9de1a41f308a337
hk-256 <harsh313275@gmail.com>
2024-09-13 01:42:37 +0530

    added distance and duration matrix

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ app.use(express.static(path.join(__dirname,"public")));
 
 
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
-const mbxDirection = require("@mapbox/mapbox-sdk/services/directions");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({accessToken: mapBoxToken});
-const geodirector = mbxDirection({accessToken: mapBoxToken});
 
 
 app.engine("ejs",ejsMate);
@@ -54,19 +52,45 @@ app.post("/add",async (req,res)=>{
 
     // const {location} = req.body;
     // res.send(location);
-    const geoData = await geocoder.forwardGeocode({
-        query: req.body.location,
-        limit: 1
-    }).send();
+    try{
+      // const address = req.body.location;
+      // var newAdd = "";
 
-    const info = new Info({
-        location : req.body.location,
-        geometry : geoData.body.features[0].geometry
-    });
-    console.log(info);
+      // for(let x of address){
+      //   if(x==' '){
+      //     newAdd+="+";
+      //   }
+      //   else{
+      //     newAdd+=x;
+      //   }
+      // }
+
+      // const url = `https://api.opencagedata.com/geocode/v1/json?q=${newAdd}&key=84b1e67fc4534823a413c4223bcba30b`;
+      
+      // const geoData = await fetch(url);
+
+      // console.log(geoData);
+
+
+        const geoData = await geocoder.forwardGeocode({
+          query: req.body.location,
+          limit: 1
+        }).send();
+
+        const info = new Info({
+            location : req.body.location,
+            geometry : geoData.body.features[0].geometry
+        });
+        console.log(info);
+        info.save();
+    }
+    catch(e){
+      console.log("error occured");
+      console.log(e);
+    }
+    
     // console.log(geoData.body.features[0].geometry);
 
-    info.save();
 
     res.redirect("/add");
 
@@ -76,20 +100,55 @@ app.use(async (req,res)=>{
 
     const info = await Info.find({});
 
-    console.log(info[0].geometry.coordinates);
-    const geoData = await geodirector.getDirections({
-        profile: 'driving-traffic',
-        waypoints:[
-            {
-                coordinates: info[0].geometry.coordinates
-            },
-            {
-                coordinates: info[1].geometry.coordinates
-            }
-        ]
-    }).send();
+        
+    var coordinatesString = "";
 
-    console.log(geoData);
-    res.render("main.ejs",{info});
+    for(let x of info){
+      coordinatesString+=x.geometry.coordinates[0];
+      coordinatesString+=",";
+      coordinatesString+=x.geometry.coordinates[1];
+      coordinatesString+=";";
+    }
+    coordinatesString = coordinatesString.substring(0, coordinatesString.length - 1);
+    const profile = 'driving';
+
+    const url = `https://api.mapbox.com/directions-matrix/v1/mapbox/${profile}/${coordinatesString}?annotations=distance,duration&access_token=${mapBoxToken}`;
+
+    // console.log(url);
+
+    
+    // console.log(info[0].geometry.coordinates);
+    // const geoData = await geodirector.getDirections({
+      //     profile: 'driving-traffic',
+      //     waypoints:[
+        //         {
+          //             coordinates: info[0].geometry.coordinates
+          //         },
+          //         {
+            //             coordinates: info[1].geometry.coordinates
+            //         }
+            //     ]
+            // }).send();
+    
+    try{
+
+      const response = await fetch(url);
+      if(!response.ok){
+        throw new Error('Network responsewas not ok');
+      }
+
+      const geoData = await response.json();
+      console.log(geoData);
+      const distances = geoData.distances , durations = geoData.durations;
+
+      res.render("main.ejs",{info,distances,durations});
+
+    }
+    catch(e){
+      // console.log("error",e);
+      res.send("some error occured in app.use accessing distance matrix");
+    }    
+    
+
 
 })
